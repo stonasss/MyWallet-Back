d84d4cdc1c024fcb0e3869ea70d29c883207f2ec
stonasss <luis.b.santos96@gmail.com>
2023-01-20 16:54:53 -0300

    server fix & instalação de biblioteca
    ajuste para possiblitar interação com db, instalação do uuid para criação de token

--- src/app.js
+++ src/app.js
@@ -4,64 +4,76 @@ import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import bcrypt from "bcrypt";
 import joi from "joi";
+import { v4 as uuidV4 } from "uuid";
 
 dotenv.config();
 const server = express();
 server.use(express.json());
 server.use(cors());
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
+let db;
 
 try {
   await mongoClient.connect();
-  let db = mongoClient.db();
+  db = mongoClient.db();
 } catch (err) {
-  console.log(err)
+  console.log(err);
 }
 
 const userSchema = joi.object({
   name: joi.string().required(),
   email: joi.string().email().required(),
   password: joi.string().required(),
-})
+  confirmPassword: joi.string().valid(joi.ref("password")).required(),
+});
 
 server.post("/cadastro", async (req, res) => {
-    const { name, email, password } = req.body
+  const { username, email, password, confirmPassword } = req.body;
 
-    const { err } = userSchema.validate({ name, email, password })
-    if (err) {
-        const errMsgs = err.details.map(err => err.message)
-        return res.status(422).send(errMsgs)
-    }
+  const { err } = userSchema.validate({
+    username,
+    email,
+    password,
+    confirmPassword,
+  });
 
-    const passwordHashed = bcrypt.hashSync(password, 10)
+  if (err) {
+    const errMsgs = err.details.map((err) => err.message);
+    return res.status(422).send(errMsgs);
+  }
 
-    try {
-        await db.collection("users").insertOne({ name, email, password: passwordHashed })
-        res.status(201).send("Usuário cadastrado")
-    } catch (err) {
-        res.status(500).send(err.message)
-    }
-})
+  const passwordHashed = bcrypt.hashSync(password, 10);
+
+  try {
+    await db.collection("users").insertOne({
+      username,
+      email,
+      password: passwordHashed,
+    });
+    res.status(201).send("Usuário cadastrado");
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
 
 server.post("/", async (req, res) => {
-    const { email, password } = req.body
+  const { email, password } = req.body;
 
-    try {
-        const userExists = await db.collection("users").findOne({ email })
-        if (!userExists) return res.status(422).send("Dados incorretos")
+  try {
+    const userExists = await db.collection("users").findOne({ email });
+    if (!userExists) return res.status(422).send("Dados incorretos");
 
-        const passwordCorrect = bcrypt.compareSync(password, userExists.password)
-        if (!passwordCorrect) {
-            return res.status(422).send("Dados incorretos")
-        }
-        return res.status(200).send("Você está logado")
+    const passwordCorrect = bcrypt.compareSync(password, userExists.password);
+    if (!passwordCorrect) return res.status(422).send("Dados incorretos");
 
-    } catch (err) {
-        res.status(500).send(err.message)
-    }
-})
+    const token = uuidV4();
+    return res.status(200).send(token);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
 
 const PORT = 5000;
 server.listen(PORT, () => {
-    console.log("Servidor aberto");
+  console.log("Servidor aberto");
 });
